d810985924f9376e2f866b4b106eed0f1f5a7d74
dslee <dslee@me-zoo.com>
2023-05-09 10:33:42 +0900

    add print

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    print("변동사항 3");
+    print("변동사항 4");
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
